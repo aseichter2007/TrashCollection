376ec8b35c7ab0876fe69458f63307c6d81ce2f4
aseichter2007 <aseichter2007@gmail.com>
2020-05-21 06:39:37 -0500

    Add project files.

---
+++ TrashCollection/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollection.Models
+{
+    public class Employee
+    {
+        [ForeignKey("User")]
+        public int Id { get; set; }
+        public User User { get; set; }
+        [ForeignKey]
+    }
+}
